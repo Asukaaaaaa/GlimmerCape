904bae7926f93e737ebc67e852fbf0e9425bbbb8
zhonghua04 <zhonghua04@meituan.com>
2020-09-15 00:28:00 +0800

    init

---
+++ config/config.ts
@@ -0,0 +1,145 @@
+// https://umijs.org/config/
+import { defineConfig } from 'umi';
+import defaultSettings from './defaultSettings';
+import proxy from './proxy';
+const { REACT_APP_ENV } = process.env;
+export default defineConfig({
+  hash: true,
+  antd: {},
+  dva: {
+    hmr: true,
+  },
+  locale: {
+    // default zh-CN
+    default: 'zh-CN',
+    antd: true,
+    // default true, when it is true, will use `navigator.language` overwrite default
+    baseNavigator: true,
+  },
+  dynamicImport: {
+    loading: '@/components/PageLoading/index',
+  },
+  targets: {
+    ie: 11,
+  },
+  // umi routes: https://umijs.org/docs/routing
+  routes: [
+    // welcome
+    {
+      path: '/welcome',
+      component: './Welcome',
+      routes: [
+        {
+          path: '/welcome',
+          name: 'welcome',
+          component: './Welcome',
+        },
+      ],
+    },
+    // user
+    {
+      path: '/user',
+      component: '../layouts/UserLayout',
+      routes: [
+        {
+          name: 'login',
+          path: '/user/login',
+          component: './user/login',
+        },
+        {
+          name: 'register',
+          path: '/user/register',
+          component: './user/Register',
+        },
+        {
+          name: 'result',
+          path: '/user/register/result',
+          component: './user/RegisterResult',
+        },
+      ],
+    },
+    // app
+    {
+      path: '/',
+      component: '../layouts/SecurityLayout',
+      routes: [
+        {
+          path: '/',
+          component: '../layouts/BasicLayout',
+          authority: ['admin', 'user'],
+          routes: [
+            {
+              path: '/',
+              redirect: '/project',
+            }, // {
+            //   path: '/welcome',
+            //   name: 'welcome',
+            //   icon: 'smile',
+            //   component: './Welcome',
+            // },
+            // {
+            //   name: 'list.table-list',
+            //   icon: 'table',
+            //   path: '/list',
+            //   component: './ListTableList',
+            // },
+            {
+              path: '/project',
+              exact: true,
+              name: 'project',
+              icon: 'smile',
+              component: './project',
+              // hideChildrenInMenu: true,
+              routes: [
+                {
+                  path: '/project/detail',
+                  exact: true,
+                  name: 'detail',
+                  icon: 'smile',
+                  component: './project/PrjDetail',
+                },
+              ],
+            },
+            {
+              path: '/admin',
+              name: 'admin',
+              icon: 'crown',
+              component: './Admin',
+              authority: ['admin'],
+              routes: [
+                {
+                  path: '/admin/sub-page',
+                  name: 'sub-page',
+                  icon: 'smile',
+                  component: './Welcome',
+                  authority: ['admin'],
+                },
+              ],
+            },
+            {
+              component: './404',
+            },
+          ],
+        },
+        {
+          component: './404',
+        },
+      ],
+    },
+    {
+      component: './404',
+    },
+  ],
+  // Theme for antd: https://ant.design/docs/react/customize-theme-cn
+  theme: {
+    // ...darkTheme,
+    'primary-color': defaultSettings.primaryColor,
+  },
+  // @ts-ignore
+  title: false,
+  ignoreMomentLocale: true,
+  proxy: proxy[REACT_APP_ENV || 'dev'],
+  manifest: {
+    basePath: '/',
+  },
+});
